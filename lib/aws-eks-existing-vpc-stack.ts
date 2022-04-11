7c7c691fc867c514a38fad3bded8d92d54ba27a2
deepayan <deepyan@amazon.com>
2022-04-11 18:48:49 +0530

    initial commit

--- lib/aws-eks-existing-vpc-stack.ts
+++ lib/aws-eks-existing-vpc-stack.ts
@@ -1,16 +1,24 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as eks from 'aws-cdk-lib/aws-eks';
 
 export class AwsEksExistingVpcStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
+		const vpc = ec2.Vpc.fromLookup(this, 'DefaultVPC', { isDefault: true });
 
-    // The code that defines your stack goes here
+		new eks.Cluster(this, 'HelloEKS', {
+			version: eks.KubernetesVersion.V1_21,
+			vpc,
+			vpcSubnets: [{ subnetType: ec2.SubnetType.PRIVATE_WITH_NAT }],
+			defaultCapacity: 2,
+			defaultCapacityInstance: ec2.InstanceType.of(ec2.InstanceClass.M6G, ec2.InstanceSize.SMALL),
+			albController: {
+				version: eks.AlbControllerVersion.V2_3_1,
+			},
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsEksExistingVpcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+
+		  });
+	}
 }
