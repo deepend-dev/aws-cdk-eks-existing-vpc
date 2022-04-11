46199d13c4eff7c3ee49587727fe42b833b6b863
deepayan <deepyan@amazon.com>
2022-04-11 17:09:01 +0530

    Initial commit

---
+++ test/aws-eks-existing-vpc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsEksExistingVpc from '../lib/aws-eks-existing-vpc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-eks-existing-vpc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsEksExistingVpc.AwsEksExistingVpcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
